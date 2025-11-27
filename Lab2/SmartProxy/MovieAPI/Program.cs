bbbf20f3475fff1ee6fadd03f7fa3420a2da4ae5
Mihaela Chiaburu <mchiaburu.1337@gmail.com>
2025-11-28 01:41:17 +0200

    variables

--- Lab2/SmartProxy/MovieAPI/Program.cs
+++ Lab2/SmartProxy/MovieAPI/Program.cs
@@ -4,14 +4,23 @@ using Microsoft.Extensions.Options;
 using MovieAPI.Repositories;
 using MovieAPI.Services;
 using MovieAPI.Settings;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
+var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var pass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+var connectionString = $"Host={host};Port=5432;Database={database};Username={user};Password={pass}";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
+
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
@@ -27,6 +36,12 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ISyncService<Movie>, SyncService<Movie>>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseAuthorization();
 app.MapControllers();
