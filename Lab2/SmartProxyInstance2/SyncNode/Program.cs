0c793dd412648d5449776b9de3217acff819ed0c
Mihaela Chiaburu <mchiaburu.1337@gmail.com>
2025-11-28 11:51:30 +0200

    inst 2 sync

--- Lab2/SmartProxyInstance2/SyncNode/Program.cs
+++ Lab2/SmartProxyInstance2/SyncNode/Program.cs
@@ -7,12 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Configureaza clasa MovieAPISettings din appsettings.json
+var movieApiDw1 = Environment.GetEnvironmentVariable("MOVIEAPI_DW1_URL")
+    ?? "http://movieapi-instance1/api";
+var movieApiDw2 = Environment.GetEnvironmentVariable("MOVIEAPI_DW2_URL")
+    ?? "http://movieapi-instance2/api";
+
+builder.Configuration["MovieAPISettings:Hosts:0"] = movieApiDw1;
+builder.Configuration["MovieAPISettings:Hosts:1"] = movieApiDw2;
+
+// Log pentru debugging
+Console.WriteLine($"Sync Node - MovieAPI DW1: {movieApiDw1}");
+Console.WriteLine($"Sync Node - MovieAPI DW2: {movieApiDw2}");
+
 builder.Services.Configure<MovieAPISettings>(
     builder.Configuration.GetSection("MovieAPISettings")
 );
 
-// Inregistreaza interfata pentru acces usor
 builder.Services.AddSingleton<IMovieAPISettings>(sp =>
     sp.GetRequiredService<IOptions<MovieAPISettings>>().Value
 );
@@ -26,4 +36,5 @@ var app = builder.Build();
 
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
